bb48e57304a616fc2af69a2d7dbbc4cb827066a3
habibullo <azimovhabibullo1@gmail.com>
2023-09-06 20:42:57 +0500

    get and post methods

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Noutbooks, Keyboard
 # Register your models here.
+
+admin.site.register(Noutbooks)
+admin.site.register(Keyboard)
--- polls/views.py
+++ polls/views.py
@@ -2,30 +2,52 @@ from django.shortcuts import render, get_object_or_404
 from .models import Noutbooks, Keyboard
 from django.http import JsonResponse
 from .serializers import NoutbookSerializer, KeyboardSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 
-def detail(request, noutbook_id):
-    noutbook = get_object_or_404(Noutbooks, id = noutbook_id)
-    noubook_data = NoutbookSerializer(noutbook)
-    
-    return JsonResponse(noubook_data.data, safe=False)
+class Detail(APIView):
+    def get(self, request, *args, **kwargs):
+        noutbook = get_object_or_404(Noutbooks, id = kwargs['noutbook_id'])
+        result = NoutbookSerializer(noutbook)
+        return Response(result.data)
 
-def all(request):
-    noutbook = Noutbooks.objects.all()
-    result = NoutbookSerializer(noutbook, many=True)
-    
-    return JsonResponse(result.data, safe=False)   
+class All(APIView):    
+    def get(self, request):
+        noutbook = Noutbooks.objects.all()
+        result = NoutbookSerializer(noutbook, many=True)
+        
+        return Response(result.data)   
 
 
-def detail2(request, keyboard_id):
-    keyboards = get_object_or_404(Keyboard, id = keyboard_id)
-    keyboard_data = KeyboardSerializer(keyboards)
-    
-    return JsonResponse(keyboard_data.data, safe=False)
+class Detail2(APIView):
+    def get(self, request, *args, **kwargs):
+        keyboards = get_object_or_404(Keyboard, id = kwargs['keyboard_id'])
+        keyboard_data = KeyboardSerializer(keyboards)
 
-def all2(request):
-    keyboards = Keyboard.objects.all()
-    result = KeyboardSerializer(keyboards, many=True)
-    
-    return JsonResponse(result.data, safe=False)      
+        return Response(keyboard_data.data)
+
+class All2(APIView):
+    def get(self, request):
+        keyboards = Keyboard.objects.all()
+        result = KeyboardSerializer(keyboards, many=True)
+
+        return Response(result.data)   
+
+class CreateNoutbookView(APIView):
+    def post(self, request):
+        user_body = request.data 
+        serializer = NoutbookSerializer(data = user_body)          
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)    
+
+class CreateKeyboardView(APIView):
+    def post(self, request):
+        user_body = request.data 
+        serializer = KeyboardSerializer(data = user_body)          
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)    
