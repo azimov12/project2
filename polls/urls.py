5815dafd6d090844bfe6dee8410d5efb8be8779d
habibullo <azimovhabibullo1@gmail.com>
2023-09-06 21:46:56 +0500

    post added to polls.urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import All, Detail, All2, Detail2
+from .views import All, Detail, All2, Detail2, CreateKeyboardView,CreateNoutbookView
 
 urlpatterns = [
     path('detail/<int:noutbook_id>', Detail.as_view()),
     path('all/', All.as_view()),
     path('detail2/<int:keyboard_id>', Detail2.as_view()),
     path('all2/', All2.as_view()),
+    path('create1/', CreateNoutbookView.as_view()),
+    path('create2/', CreateKeyboardView.as_view()),
 ]
